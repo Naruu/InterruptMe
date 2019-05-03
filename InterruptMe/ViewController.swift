cf64b48f7acdc915eaaf29690828c26ac36ebe74
Naruu <esara2021@gmail.com>
2019-05-03 18:14:59 +0900

    .gitignore 추가. 프로젝트 시작.

--- InterruptMe/ViewController.swift
+++ InterruptMe/ViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewContr oller {
 
+    
+    @IBOutlet weak var timeSetterView : UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
