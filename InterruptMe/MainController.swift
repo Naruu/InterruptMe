980b891849c74a599e80c8a7a7da8fae1f58a328
Naruu <esara2021@gmail.com>
2019-05-14 22:07:55 +0900

    change color

--- InterruptMe/MainController.swift
+++ InterruptMe/MainController.swift
@@ -16,7 +16,7 @@ class MainController: UIViewController {
         picker.datePickerMode = UIDatePicker.Mode.countDownTimer
         picker.minuteInterval = 10
         picker.countDownDuration = 24*60
-        picker.backgroundColor = UIColor.white
+        picker.backgroundColor = .white
         
         return picker
     }()
@@ -26,6 +26,8 @@ class MainController: UIViewController {
         titleView.text = "Set the timer"
         titleView.font = UIFont.boldSystemFont(ofSize: 25)
         titleView.translatesAutoresizingMaskIntoConstraints = false
+        titleView.backgroundColor = UIColor(red: 41/255, green:71/255, blue:131/255, alpha:1)
+        titleView.textColor = .white
         titleView.textAlignment = .center
         return titleView
     }()
@@ -33,25 +35,29 @@ class MainController: UIViewController {
     let oneButton: UIButton = {
         let button = UIButton(type: UIButton.ButtonType.system)
         button.setTitle("Start", for: .normal)
-        button.setTitleColor(UIColor.white, for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
 
-        //button.clipsToBounds = true
-        button.backgroundColor = .blue
+        button.layer.cornerRadius = 50
+        button.layer.masksToBounds = true
+        button.backgroundColor = .white
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(buttonTapped), for:.touchUpInside)
         return button
     }()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.title = "Timer"
         
-        view.backgroundColor = UIColor.white
+        view.backgroundColor = UIColor(red: 41/255, green:71/255, blue:131/255, alpha:1)
         
         self.view.addSubview(timerPicker)
         self.view.addSubview(viewTitle)
         self.view.addSubview(oneButton)
         setupLayout()
         timerPicker.addTarget(self, action: #selector(self.changed(_:)), for: .valueChanged)
+    
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -79,6 +85,18 @@ class MainController: UIViewController {
         let duration = timerPicker.countDownDuration
     }
 
+    @objc private func buttonTapped(){
+        /*if(!oneButton.clicked){
+            oneButton.clicked = true
+            oneButton.setTitle("Stop", for:.normal)
+        }
+        else {
+            oneButton.clicked = false
+            oneButton.setTitle("Start", for: .normal)
+        }*/
+        print("Button Tapped")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
