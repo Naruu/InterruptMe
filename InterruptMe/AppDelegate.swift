b43c2dc62260f98a12c56cdefd0c15cc161fa8ee
Naruu <esara2021@gmail.com>
2019-05-24 20:03:10 +0900

    Detection of screen lock. Not implemented yet. More studies on CFNotificationCenter needed

--- InterruptMe/AppDelegate.swift
+++ InterruptMe/AppDelegate.swift
@@ -31,9 +31,27 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             application.registerUserNotificationSettings(setting)
         }
         
-        
         return true
     }
+    
+    func registerforDeviceLockNotification() {
+        
+        CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(),
+                                        nil,
+            screenLockChanged,
+            "com.apple.springboard.lockcomplete" as CFString,
+            nil,     // object
+            .deliverImmediately)
+        
+        CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(),
+            nil,
+            screenLockChanged,
+            "com.apple.springboard.lockstate" as CFString,
+            nil,
+            .deliverImmediately)
+    }
+   
+
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
