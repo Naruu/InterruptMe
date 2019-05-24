b07feef998a498cb62e6480e0ee4daa585b65a7e
Naruu <esara2021@gmail.com>
2019-05-24 20:05:17 +0900

    Callback updated

--- InterruptMe/AppDelegate.swift
+++ InterruptMe/AppDelegate.swift
@@ -51,7 +51,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             .deliverImmediately)
     }
    
-
+    let screenLockChanged: CFNotificationCallback = { center, observer, name, object, info in
+        
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
