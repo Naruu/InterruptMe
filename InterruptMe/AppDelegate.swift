c4ce27740e34e3cc34d4bbe191390d55725abd1a
Naruu <esara2021@gmail.com>
2019-06-20 02:58:18 +0900

    correct typo

--- InterruptMe/AppDelegate.swift
+++ InterruptMe/AppDelegate.swift
@@ -40,7 +40,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         
-        let token = deviceToken.map{ String(format: "%02.2hhx", $0)}.joined()/Users/release/20151341/InterruptMe/InterruptMe/AppDelegate.swift
+        let token = deviceToken.map{ String(format: "%02.2hhx", $0)}.joined()
         print(token)
         //self.sendDevicdTokenToServer(data: deviceToken)
     }
